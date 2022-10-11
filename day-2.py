7f5c64185393ff3a6d4dd7557ce942671ce9da9d
yakusoku_suru <okedelep@gmail.com>
2022-10-11 14:32:33 +0100

    day 2

---
+++ day-2.py
@@ -0,0 +1,11 @@
+# Variables
+a = 2
+b = "John"
+c = 5
+print(a, b)
+print("The data type of a is ", type(a))
+print("The data type of b is ", type(b))
+print("Concatenating the variables a and b gives: ", str(a) + b)
+print("Addition of a and c gives: ", a + c)
+print (a > c)
+print(c > a or a > c)
