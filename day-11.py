6b6dc9e59c34e2ae1985d6a323e58632888d6f6f
yakusoku_suru <okedelep@gmail.com>
2022-10-20 21:40:50 +0100

    day 11

---
+++ day-11.py
@@ -0,0 +1,14 @@
+# RANDOM NUMBER FILE WRITER
+import random
+
+def random_number_file_writer():
+    no_of_random_numbers = int(
+        input("Specify how many random numbers the file will hold: ")
+    )
+    for i in range(no_of_random_numbers):
+        random_number = random.randint(1, 500)
+        file = open("randomNumberFile", "a")
+        file.write(f'{random_number}\n')
+    print("Process completed......")
+
+random_number_file_writer()
